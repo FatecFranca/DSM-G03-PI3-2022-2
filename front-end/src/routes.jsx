7201e669fe5bfa8437ce3b378ec0e9109365ab8f
xjapax <jvv.joaovitor559@gmail.com>
2022-11-09 23:00:21 -0300

    iniciando rotas front end

---
+++ front-end/src/routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
+export default function Routers() {
+    return (
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    );
+}
+
+
+
