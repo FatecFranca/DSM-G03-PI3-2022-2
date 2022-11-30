78f7a2fce93394e7c559a7e20199047f7249755c
jaummm1 <88463022+jaummm1@users.noreply.github.com>
2022-11-30 00:33:14 -0300

    ajustes

---
+++ front-end/src/pages/Glossario/glossario.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./glossario.css";
+import NavCadastro from "../../components/navCadastro/nav";
+
+//Pagina Questoes da Aplicação
+export default function Glossario() {
+  return (
+    <div className="app">
+      <NavCadastro />
+
+      <div className="glossario"></div>
+      <div className="conteudo"></div>
+      <div className="cont">
+        <h2 style={{ fontSize: "45px" }}> Glossario </h2>
+        <p>
+          O glossário aqui apresentado não tem a intenção de ser uma obra de
+          referência completa. O seu objetivo é esclarecer os significados de
+          alguns termos usados nesse trabalho. Um termo pode ser incluído nesse
+          glossário, se utilizado inconsistentemente na literatura em geral, ou
+          se o seu significado nesse trabalho for mais específico do que o
+          normal.
+        </p>
+        <br />
+        <h2>Condução</h2>
+        <p>
+          A condução refere-se aos meios disponíveis para aconselhar, orientar,
+          informar e conduzir o usuário na interação com o computador
+          (mensagens, alarmes, rótulos, etc.). Quatro subcritérios participam da
+          condução: a presteza, o agrupamento/distinção entre itens, o feedback
+          imediato e a legibilidade.
+        </p>
+        <h2>Presteza</h2>
+        <p>
+          Esse critério engloba os meios utilizados para levar o usuário a
+          realizar determinadas ações, como, por exemplo, entrada de dados. Esse
+          critério engloba também todos os mecanismos ou meios que permitem ao
+          usuário conhecer as alternativas, em termos de ações, conforme o
+          estado ou contexto nos quais ele se encontra. A presteza diz respeito
+          igualmente às informações que permitem ao usuário identificar o estado
+          ou contexto no qual ele se encontra, bem como as ferramentas de ajuda
+          e seu modo de acesso.
+        </p>
+        <h2>Agrupamento/Distinção de Itens</h2>
+        <p>
+          O critério Agrupamento/Distinção de Itens diz respeito à organização
+          visual dos itens de informação relacionados uns com os outros de
+          alguma maneira. Esse critério leva em conta a localização e algumas
+          características gráficas (formato) para indicar as relações entre os
+          vários itens mostrados, para indicar se eles pertencem ou não a uma
+          dada classe, ou ainda para indicar diferenças entre classes. Esse
+          critério também diz respeito à organização dos itens de uma classes. O
+          critério agrupamento/distinção de itens está subdividido em dois
+          critérios: agrupamento/distinção por localização e
+          agrupamento/distinção por formato.
+        </p>
+      </div>
+    </div>
+  );
+}
--- front-end/src/routes.jsx
+++ front-end/src/routes.jsx
@@ -4,19 +4,18 @@ import Home from "./pages/Home/Home";
 import Avaliacao from "./pages/avaliaçao/avaliacao";
 import Cadastro from "./pages/Cadastro/cadastro";
 import Login from "./pages/Login/login";
-import Sobre from "./pages/Sobre/Sobre"
+import Sobre from "./pages/Sobre/Sobre";
+import Glossario from "./pages/Glossario/glossario";
 
 export default function Routers() {
-    return (
-      <Routes>
-        <Route path="/Home" element={<Home/>}/>
-        <Route path="/Avaliacao" element={<Avaliacao/>}/>
-        <Route path="/Cadastro" element={<Cadastro/>}/>
-        <Route path="/Sobre" element={<Sobre/>}/>
-        <Route path="/" element={<Login/>}/>
-      </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Avaliacao" element={<Avaliacao />} />
+      <Route path="/Cadastro" element={<Cadastro />} />
+      <Route path="/Sobre" element={<Sobre />} />
+      <Route path="/Glossario" element={<Glossario />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 }
-
-
-
